a0aabeeca83c35a44206582adf616f003e6a6c2e
DevTae <dev.taehyeon.kim@gmail.com>
2023-09-29 14:43:46 +0900

    fix #2 Update codes to transfer each data properly

--- main.py
+++ main.py
@@ -4,9 +4,26 @@ from data_crawler import data_crawler
 from price_analyzer import price_analyzer
 
 if __name__ == "__main__":
-    data_queue = queue.Queue()
-    analyzer = price_analyzer(data_queue)
-    datas = data_crawler(data_queue)
+    
+    # { period : [ limit, milliseconds ] } (limit <= 200)
+    supported_period = { "5m" : [ 100,  5000], # 300000
+                         "15m" : [ 100, 900000 ],
+                         "1H" : [ 100, 3600000 ],
+                         "6H" : [ 100, 21600000 ],
+                         "1D" : [ 100, 86400000 ] }
+        
+    # { period : [ datas, lasttime ] }
+    download_format = { "5m" : [ None, None ],
+                        "15m" : [ None, None ],
+                        "1H" : [ None, None ],
+                        "6H" : [ None, None ],
+                        "1D" : [ None, None ] }  
+    
+    supported_ticker = { "BTCUSDT_UMCBL" }
+
+    data_queue = queue.Queue() # data_crawler -> price_analyzer
+    analyzer = price_analyzer(data_queue, download_format)
+    datas = data_crawler(data_queue, supported_ticker, supported_period, download_format)
     analyzer.start()
     datas.start()
 
--- price_analyzer.py
+++ price_analyzer.py
@@ -2,16 +2,19 @@ import queue
 import threading
 
 class price_analyzer(threading.Thread):
-    def __init__(self, data_queue):
+    def __init__(self, data_queue: queue.Queue, download_format: dict):
         threading.Thread.__init__(self)
         self.data_queue = data_queue
+        self.download_format = download_format
         self.running = True
     
     def run(self):
         while self.running:
             if self.data_queue.qsize() > 0:
-                ticker, period, datas = self.data_queue.get()
-                print("[analyzer]", ticker, period, datas)
+                ticker, download_prices = self.data_queue.get()
+                print(ticker)
+                for period in self.download_format.keys():
+                    print(period, download_prices[period][0])
 
     def stop(self):
         self.running = False
